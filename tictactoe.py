fdd3e16d9abb3b08c96fd6f38b55961efe56ccbb
Kelan Mazey <kelanmazey1@gmail.com>
2021-08-11 11:06:31 +0100

    gets and validates user input and updates board

--- tictactoe.py
+++ tictactoe.py
@@ -6,24 +6,57 @@
 ######################################################
 
 # Display the board
-def display_board():
-  pos1 = pos2 = pos3 = pos4 = pos5 = pos6 = pos7 = pos8 = pos9 = '-'
+def display_board(board_values):
   print(f'\n     |     |     ')
-  print(f'  {pos1}  |  {pos2}  |  {pos3}  ')
+  print(f'  {board_values[0]}  |  {board_values[1]}  |  {board_values[2]}  ')
   print(f'_____|_____|_____')
   print(f'     |     |     ')
-  print(f'  {pos4}  |  {pos5}  |  {pos6}  ')
+  print(f'  {board_values[3]}  |  {board_values[4]}  |  {board_values[5]}  ')
   print(f'_____|_____|_____')
   print(f'     |     |     ')
-  print(f'  {pos7}  |  {pos8}  |  {pos9}  ')
+  print(f'  {board_values[6]}  |  {board_values[7]}  |  {board_values[8]}  ')
   print(f'     |     |     \n')
+
 # Get users position choice
+def get_position_choice(board_values):
+  #Placeholder value
+  valid_position = False
+  board_positions = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+  
+  while not valid_position:
+    user_input = input("Select a space (1 - 9): ")
+
+    # Check if position is already taken and if is in range
+    if user_input in board_positions:
+      integer_input = int(user_input)
+      
+      if board_values[integer_input - 1] == '-':
+        valid_position = True
+        # Return index position to be used for board updating
+        return integer_input - 1
+      else:
+        print("Oops that space is already taken!")
+
+    else:
+      print("That number isn't on the board! Please select a number between 1 and 9\n")
+
+# Update board
+def update_board(position_index, player_character, board_values):
+  board_values[position_index] = player_character
+  return board_values
+
+
+# Check if game is won 
+
 
 # Decide whether to play again
 
 
 # Main game loop
-display_board()
+# Create initial list of values for the board
+board_values = ['-'] * 9
+get_position_choice(board_values)
+display_board(board_values)
 # Choose X or O
 
 # Gather choice, display updated board
