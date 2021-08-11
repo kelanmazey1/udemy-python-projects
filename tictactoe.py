57f0c8181059e52294a31aa69a567c0fcf70fe7f
Kelan Mazey <kelanmazey1@gmail.com>
2021-08-11 10:40:40 +0100

    initial commit, added ascii board

---
+++ tictactoe.py
@@ -0,0 +1,33 @@
+######################################################
+#                                                    #
+# Tic tac toe, displayed in the command line to be   #
+# played by 2 players                                #                  
+#                                                    #
+######################################################
+
+# Display the board
+def display_board():
+  pos1 = pos2 = pos3 = pos4 = pos5 = pos6 = pos7 = pos8 = pos9 = '-'
+  print(f'\n     |     |     ')
+  print(f'  {pos1}  |  {pos2}  |  {pos3}  ')
+  print(f'_____|_____|_____')
+  print(f'     |     |     ')
+  print(f'  {pos4}  |  {pos5}  |  {pos6}  ')
+  print(f'_____|_____|_____')
+  print(f'     |     |     ')
+  print(f'  {pos7}  |  {pos8}  |  {pos9}  ')
+  print(f'     |     |     \n')
+# Get users position choice
+
+# Decide whether to play again
+
+
+# Main game loop
+display_board()
+# Choose X or O
+
+# Gather choice, display updated board
+
+# Check if game won
+
+# Check if want to play agian
